eb95855771bd8a6c92fea5885de594eafb812698
pritampodder12 <47752925+pritampodder12@users.noreply.github.com>
2020-06-17 22:31:01 +0530

    validation complete without disableButton

--- lib/mixins/validation_mixin.dart
+++ lib/mixins/validation_mixin.dart
@@ -1,9 +1,11 @@
 class ValidationMixin {
   final Function(String) emailValidator = (String value) {
+    if (value.isEmpty) return 'Email is required';
     if (!value.contains('@')) return 'Please enter valid email id';
   };
 
   final Function(String) passwordValidator = (String value) {
+    if (value.isEmpty) return 'Password is required';
     if (value.length < 3) return 'Password must atleast 3 character';
   };
 }
